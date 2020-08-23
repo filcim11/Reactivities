5267d4ba44c05c316e4f89cd4d665c2865b04021
Filip Cimbaljević <cimbaljevic.filip@gmail.com>
2020-08-23 16:08:18 +0200

    End of Section 7

--- client-app/src/app/layout/LoadingComponent.tsx
+++ client-app/src/app/layout/LoadingComponent.tsx
@@ -1,7 +1,8 @@
 import React from "react";
 import { Dimmer, Loader } from "semantic-ui-react";
+import { observer } from "mobx-react-lite";
 
-export const LoadingComponent: React.FC<{
+const LoadingComponent: React.FC<{
   inverted?: boolean;
   content?: string;
 }> = ({ inverted = true, content }) => {
@@ -11,3 +12,5 @@ export const LoadingComponent: React.FC<{
     </Dimmer>
   );
 };
+
+export default observer(LoadingComponent);
