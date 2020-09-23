988ce6ee68eee36a593271c5209aeb3c049f5d42
Filip Cimbaljević <cimbaljevic.filip@gmail.com>
2020-09-23 20:57:46 +0200

    End of Section 18

--- client-app/src/app/stores/profileStore.ts
+++ client-app/src/app/stores/profileStore.ts
@@ -98,4 +98,18 @@ export default class ProfileStore {
             })
         }
     }
+
+    @action editProfile = async (profile: Partial<IProfile>) => {
+        try {
+            await agent.Profiles.editProfile(profile!);
+            runInAction(() => {
+                if (profile.displayName !== this.rootStore.userStore.user!.displayName) {
+                    this.rootStore.userStore.user!.displayName = profile.displayName!;
+                }
+                this.profile = {...this.profile!, ...profile}
+            })
+        } catch (error) {
+            toast.error('Problem editing profile');
+        }
+    }
 }
